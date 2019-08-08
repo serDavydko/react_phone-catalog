d1e98df1ad6a4aadcb576d5af57ba86e7c282672
Serhii <davydko.serhiy@gmail.com>
2019-08-08 15:15:57 +0300

    Updates

---
+++ precache-manifest.952daea28828d8d7a84b866a36c14345.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88bcde07f002b8566158ad606a1565a8",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "0f7497bef1a5a053aa3c",
+    "url": "/react_phone-catalog/static/css/main.ee17a66a.chunk.css"
+  },
+  {
+    "revision": "6a1c4bbab50efdcfa17e",
+    "url": "/react_phone-catalog/static/js/2.954e1442.chunk.js"
+  },
+  {
+    "revision": "0f7497bef1a5a053aa3c",
+    "url": "/react_phone-catalog/static/js/main.70800fc9.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  }
+]);
