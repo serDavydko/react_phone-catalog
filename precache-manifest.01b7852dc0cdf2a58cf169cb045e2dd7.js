9b71e897e175a0dc7d67a5cff7a75af2f8193402
Serhii <davydko.serhiy@gmail.com>
2019-08-13 15:50:26 +0300

    Updates

---
+++ precache-manifest.01b7852dc0cdf2a58cf169cb045e2dd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3f6a7eb9f975387b361244ac474476",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "701b2865c5fa43c97a86",
+    "url": "/react_phone-catalog/static/css/main.f751bc8d.chunk.css"
+  },
+  {
+    "revision": "78af894279dfd30eeaf9",
+    "url": "/react_phone-catalog/static/js/2.4ca575d4.chunk.js"
+  },
+  {
+    "revision": "701b2865c5fa43c97a86",
+    "url": "/react_phone-catalog/static/js/main.84cf0270.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_phone-catalog/precache-manifest.b6e93d84c941d2a9c0dba818148ebbf2.js"
+  "/react_phone-catalog/precache-manifest.01b7852dc0cdf2a58cf169cb045e2dd7.js"
 );
 
 self.addEventListener('message', (event) => {
